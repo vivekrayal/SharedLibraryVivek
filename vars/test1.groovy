118ac9b336da480c56009e52158f8b30d80b1c42
vivekrayal <115876604+vivekrayal@users.noreply.github.com>
2023-03-12 22:12:56 +0530

    Create test1.groovy

---
+++ vars/test1.groovy
@@ -0,0 +1,15 @@
+def add(a,b){
+  echo "This is check add"
+  echo "addition of ${a} and ${b} is ${a+b}"
+}
+
+def mul(a,b){
+  echo "This is check mul"
+  echo "mul of ${a} and ${b} is ${a*b}"
+}
+
+def teamName(){
+  echo "This is SnatakF4"
+
+}
+
